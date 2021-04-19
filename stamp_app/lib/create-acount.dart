05f6810d7889435912f62a889c89e33475d447b9
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2021-04-19 17:02:03 +0200

    almost done with form

--- stamp_app/lib/create-acount.dart
+++ stamp_app/lib/create-acount.dart
@@ -10,24 +10,146 @@ class CreateAccount extends StatefulWidget {
 class CreateAccountState extends State<CreateAccount> {
   // State parameter
   String _fullName;
+  String _email;
+  String _mobilnumber;
+  String _userPassword;
 
   // key to hold the state of the form i.e referens to the form
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildName() {
-    return TextFormField(
-      // Decorate the input field here
-      decoration: InputDecoration(labelText: 'Förnamn Efternamn'),
-      // The acutal value from the input
-      validator: (String value) {
-        if (value.isEmpty) {
-          return 'namn är obligatorisk';
-        }
-      },
-      // The  form is saved and we tell what to do with the value
-      onSaved: (String value) {
-        _fullName = value;
-      },
+    return Container(
+      width: 350,
+      child: TextFormField(
+        keyboardType: TextInputType.name,
+        maxLength: 40,
+        // Decorate the input field here,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Förnamn Efternamn',
+          counterStyle: TextStyle(color: Colors.white),
+        ),
+        // The acutal value from the input
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Namn är obligatorisk';
+          }
+        },
+        // The  form is saved and we tell what to do with the value
+        onSaved: (String value) {
+          _fullName = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildEmail() {
+    return Container(
+      width: 350,
+      child: TextFormField(
+        keyboardType: TextInputType.emailAddress,
+        maxLength: 40,
+        // Decorate the input field here,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'E-post',
+          counterStyle: TextStyle(color: Colors.white),
+        ),
+        // The acutal value from the input
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'E-post är obligatorisk';
+          }
+
+          if (!RegExp(
+                  r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+              .hasMatch(value)) {
+            return 'Ogiltig mejladress';
+          }
+          return null;
+        },
+        // The  form is saved and we tell what to do with the value
+        onSaved: (String value) {
+          _email = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildNumber() {
+    return Container(
+      width: 350,
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        // Decorate the input field here,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Telefonnummer (Frivilligt)',
+          counterStyle: TextStyle(color: Colors.white),
+        ),
+
+        // The  form is saved and we tell what to do with the value
+        onSaved: (String value) {
+          _mobilnumber = value;
+        },
+      ),
+    );
+  }
+
+  Widget _buildPassword() {
+    return Container(
+      width: 350,
+      child: TextFormField(
+        keyboardType: TextInputType.visiblePassword,
+        // Decorate the input field here,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Lösenord',
+          counterStyle: TextStyle(color: Colors.white),
+        ),
+        // The acutal value from the input
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Lösenord är obligatorisk';
+          }
+          return null;
+        },
+        // The  form is saved and we tell what to do with the value
+        onSaved: (String value) {
+          _userPassword = value;
+        },
+      ),
+    );
+  }
+
+  Widget _checkUserPassword() {
+    return Container(
+      width: 350,
+      child: TextFormField(
+        keyboardType: TextInputType.visiblePassword,
+        // Decorate the input field here,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Upprepa lösenord',
+          counterStyle: TextStyle(color: Colors.white),
+        ),
+        // The acutal value from the input
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Upprepa lösenord är obligatorisk';
+          }
+          if (value != _userPassword) {
+            // TODO: Change the text below
+            return 'Lösenord är samma';
+          }
+          return null;
+        },
+      ),
     );
   }
 
@@ -64,9 +186,19 @@ class CreateAccountState extends State<CreateAccount> {
                   ),
                 ),
                 SizedBox(
-                  height: 100,
+                  height: 50,
                 ),
                 _buildName(),
+                _buildEmail(),
+                _buildNumber(),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                ),
+                _buildPassword(),
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                ),
+                _checkUserPassword(),
                 // adding space
                 SizedBox(
                   height: 100,
@@ -83,6 +215,7 @@ class CreateAccountState extends State<CreateAccount> {
                     }
 
                     // If the form is valid, onSaved method is called
+                    // onsave method from above is called
                     _formKey.currentState.save();
                     print(_fullName);
                   },
@@ -102,38 +235,3 @@ class CreateAccountState extends State<CreateAccount> {
     );
   }
 }
-
-/*class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red.shade900,
-        appBar: AppBar(
-          title: Image.asset('assets/images/uuLogaNew.png', fit: BoxFit.cover),
-          centerTitle: true,
-          backgroundColor: Colors.red.shade900,
-          elevation: 0,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back_ios_rounded),
-            // TODO: Change this
-            onPressed: () => Navigator.of(context).pop(),
-            tooltip: 'Tillbaka',
-          ),
-        ),
-        body: Container(
-          width: double.infinity,
-          margin: EdgeInsets.only(top: 60, bottom: 20),
-          child: Text(
-            'Registrera Konto',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}*/
