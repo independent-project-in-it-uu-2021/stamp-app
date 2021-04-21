cbc9d20417eed044dcdd9c971d3b268a0cbea8aa
Daniel <danno-082011@hotmail.com>
2021-04-21 10:14:04 +0200

    Created server file

---
+++ server/server.py
@@ -0,0 +1,5 @@
+from firebase import firebase
+firebase=firebase = firebase.FirebaseApplication('https://stamp-db6ad-default-rtdb.europe-west1.firebasedatabase.app/', None)#https://your_storage.firebaseio.com
+result = firebase.get('/users', None)
+#http://ozgur.github.io/python-firebase/
+#eur3
