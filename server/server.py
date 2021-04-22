5c75b2ed48cbe450f572b01e8c70d491a598890e
Daniel <danno-082011@hotmail.com>
2021-04-22 20:23:53 +0200

    added simple test for inserting user

--- server/server.py
+++ server/server.py
@@ -3,3 +3,30 @@ firebase= firebase.FirebaseApplication('https://stamp-db6ad-default-rtdb.europe-
 result = firebase.get('/users', None)
 #http://ozgur.github.io/python-firebase/
 #eur3
+
+data = {
+    'name': 'user1',
+    'email': 'user1@student.uu.se',
+    'phone': 46701234567,
+    'pwd': 'abc123',
+    'pwd_confirm': 'abc123', #bool?
+    'program': 'IT',
+    'image': 'img_url',
+}
+
+result = firebase.post('/stamp-db6ad-default-rtdb/student', data)
+print(result)
+
+
+
+#def wri(dest):
+#    from firebase_admin import db
+
+#    ref = db.reference("/") #Setting ref to db root
+
+#    import json
+#    with open(dest, "r") as f:
+#        file_cont = json.load(f)
+#    ref.set(file_cont)
+    #https://www.freecodecamp.org/news/how-to-get-started-with-firebase-using-python/
+    
