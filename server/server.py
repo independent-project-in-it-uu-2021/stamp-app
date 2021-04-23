ef1ad61ab0598aedc60f0dc29e46e666983e6842
Daniel <danno-082011@hotmail.com>
2021-04-23 10:04:38 +0200

    nothing new just updating

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
 from firebase import firebase
 firebase= firebase.FirebaseApplication('https://stamp-db6ad-default-rtdb.europe-west1.firebasedatabase.app/', None)#https://your_storage.firebaseio.com
-result = firebase.get('/users', None)
+retrive = firebase.get('/stamp-db6ad-default-rtdb/student', None)
+print(retrive)
+
 #http://ozgur.github.io/python-firebase/
 #eur3
 
