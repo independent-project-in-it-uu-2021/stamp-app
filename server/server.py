c4a61e2118644495246eaf424e6d7765a38fd9cc
Daniel <danno-082011@hotmail.com>
2021-04-23 16:44:00 +0200

    start of sdk + key

--- server/server.py
+++ server/server.py
@@ -1,11 +1,24 @@
 from firebase import firebase
+#from firebase_admin import firebase
+import firebase_admin
 from firebase_admin import db
 #import json
+
+
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("/home/daniel/kex/stamp-app/stamp-db6ad-firebase-adminsdk-tu6j1-5f9c6e5cea.json")
+#create SDK:
+firebase_admin.initialize_app(cred)
+
+
+#Set db url
 firebase= firebase.FirebaseApplication('https://stamp-db6ad-default-rtdb.europe-west1.firebasedatabase.app/', None)
 #https://your_storage.firebaseio.com
 location = '/stamp-db6ad-default-rtdb/student/'
 #WRITING data TO SERVER
-data = {
+data = {"Gholam":{
     'name': 'user1',
     'email': 'user1@student.uu.se',
     'phone': 46701234567,
@@ -13,8 +26,8 @@ data = {
     'pwd_confirm': 'abc123', #bool?
     'program': 'IT',
     'image': 'img_url',
-}   
-
+    }   
+}
 result = firebase.post(location, data)
 print(result)    
 
