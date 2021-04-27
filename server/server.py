222e7fc3545617a8b89fd2c4d1333d3af2a390e1
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2021-04-27 17:02:39 +0200

    Post and get works

--- server/server.py
+++ server/server.py
@@ -1,22 +1,38 @@
 from firebase import firebase
 #from firebase_admin import firebase
-import firebase_admin
-from firebase_admin import db
+#import firebase_admin
+#from firebase_admin import db
 #import json
 
 
-import firebase_admin
-from firebase_admin import credentials
+#import firebase_admin
+#from firebase_admin import credentials
 
-cred = credentials.Certificate("/git home/daniel/kex/stamp-app/stamp-db6ad-firebase-adminsdk-tu6j1-5f9c6e5cea.json")
+#cred = credentials.Certificate("/git Users/gholam/development/stamp-app-repo/stamp-db6ad-firebase-adminsdk-tu6j1-5f9c6e5cea.json")
 #create SDK:
-firebase_admin.initialize_app(cred)
+#firebase_admin.initialize_app(cred)
 
-
+print('I am here')
+data = {
+    'username': 'Gholam',
+    'email': 'gholam@gholam.se',
+    'program': 'IT',
+    'password': '420420'
+}
 #Set db url
-firebase= firebase.FirebaseApplication('https://stamp-db6ad-default-rtdb.europe-west1.firebasedatabase.app/', None)
+firebase = firebase.FirebaseApplication('https://stamp-db6ad-default-rtdb.europe-west1.firebasedatabase.app/', None)
+#result = firebase.put('/stamp-db6ad-default-rtdb', 'gholam', data)
+#print(result)
+result = firebase.get('/stamp-db6ad-default-rtdb', 'gholam')
+print(result['email'])
+#print(result)
 #https://your_storage.firebaseio.com
-location = '/stamp-db6ad-default-rtdb/student/'
+#location = '/stamp-db6ad-default-rtdb/student/'
+
+
+
+
+"""
 #WRITING data TO SERVER
 data = {"Gholam":{
     'name': 'user1',
@@ -28,18 +44,21 @@ data = {"Gholam":{
     'image': 'img_url',
     }   
 }
-result = firebase.post(location, data)
-print(result)    
+"""
+#result = firebase.post(location, data)
+#print(result)    
 
 #RETRIVEING ALL DATA FROM SERVER
 
-retrive = firebase.get(location, None)
-print(retrive)
+
+#retrive = firebase.get(location, 'user2@student.uu.se')
+#retrive = firebase.get('/student', 'user2@student.uu.se')
+#print(retrive)
 
 #UPDATING DATABASE FROM SERVER
-test_user_id = 'MYyZHu3C2EAqdD8TzGx'
-firebase.put(location + test_user_id, 'name', 'Gholam')
+#test_user_id = 'MYyZHu3C2EAqdD8TzGx'
+#firebase.put(location + test_user_id, 'name', 'Gholam')
 
 #DELETING DATA FROM DATABASE
-test_del_id = '-MYyZ-oXjvMBI74gsEfw'
-firebase.delete(location, test_del_id)
+#test_del_id = '-MYyZ-oXjvMBI74gsEfw'
+#firebase.delete(location, test_del_id)
