cf0c3604b233d7f9694de51db85e8e2217c1b803
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2021-04-28 22:24:56 +0200

    working on with the server and authentication

--- stamp_app/lib/main.dart
+++ stamp_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stamp_app/screens/wrapper.dart';
 import './loggain.dart';
 
 import './create-acount.dart';
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Wrapper(),
       title: 'Registrera konto',
       //home: CreateAccount(),
     );
