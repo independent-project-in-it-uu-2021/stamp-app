1806ab9ce8821b3751f9c17c405ea6f5c0f1c8c8
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2021-05-11 22:14:56 +0200

    solved conflict

--- stamp_app/lib/screens/home/home.dart
+++ stamp_app/lib/screens/home/home.dart
@@ -1,34 +1,25 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:stamp_app/services/auth.dart';
-import 'package:stamp_app/services/database.dart';
 
 class Home extends StatelessWidget {
   final AuthService _firebaseAuth = AuthService();
   @override
   Widget build(BuildContext context) {
     // Streamproivder listen to stream and exposes its content to child
-    return StreamProvider<QuerySnapshot>.value(
-      // TODO: Take care of the null value below
-      initialData: null,
-      value: DatabaseService().curDocument,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Hem'),
-          backgroundColor: Colors.red.shade900,
-          actions: <Widget>[
-            FlatButton.icon(
-              onPressed: () async {
-                await _firebaseAuth.signOutUser();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Logga ut'),
-            )
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hem'),
+        backgroundColor: Colors.red.shade900,
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: () async {
+              await _firebaseAuth.signOutUser();
+            },
+            icon: Icon(Icons.person),
+            label: Text('Logga ut'),
+          )
+        ],
       ),
     );
   }
