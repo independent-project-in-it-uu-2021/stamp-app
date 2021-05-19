f748441f3e59db31780eb146b7c288ec03019458
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2021-05-19 16:57:58 +0200

    Routing from home to profile works now

--- stamp_app/lib/screens/home/home.dart
+++ stamp_app/lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:stamp_app/services/auth.dart';
+import 'package:stamp_app/screens/profil/profil.dart';
 
 class Home extends StatelessWidget {
   final AuthService _firebaseAuth = AuthService();
@@ -20,6 +21,32 @@ class Home extends StatelessWidget {
             label: Text('Logga ut'),
           )
         ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.grey,
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  'Profil',
+                  style: TextStyle(
+                    fontSize: 33,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                enabled: true,
+                contentPadding: EdgeInsets.only(left: 18.0, top: 20.0),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Profil()),
+                  );
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
