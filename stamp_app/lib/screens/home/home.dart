bb622f60f2a27e99d1464b77185142b592ba489b
Ella Hallgren <ellahallgren@MacBook-Pro-som-tillhor-Daniel.local>
2021-05-28 14:59:20 +0200

    typ klar utseende mässigt:)

--- stamp_app/lib/screens/home/home.dart
+++ stamp_app/lib/screens/home/home.dart
@@ -6,6 +6,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red.shade900,
       appBar: AppBar(
         elevation: 5,
         backgroundColor: Colors.red.shade900,
@@ -18,19 +19,161 @@ class Home extends StatelessWidget {
               icon: Icon(Icons.person),
               label: Text('Logga ut'),
             ),
-            Text('Meny'),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            ),
+            Text(
+              'Meny',
+              style: TextStyle(
+                fontSize: 33,
+              ),
+            ),
             FlatButton.icon(
-              padding: EdgeInsets.fromLTRB(150, 0, 0, 0),
+              padding: EdgeInsets.fromLTRB(85, 0, 0, 0),
               onPressed: () async {
                 await _firebaseAuth.signOutUser();
               },
-              icon: Icon(Icons.chat_bubble_rounded),
+              icon: Icon(
+                Icons.chat_bubble_rounded,
+                color: Colors.white,
+                size: 35,
+              ),
               label: Text(''),
             ),
           ],
         ),
       ),
-      backgroundColor: Colors.red.shade900,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 0, 0, 0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+            ),
+            //Button to go to Job page
+            Container(
+              height: 100,
+              width: 330,
+              child: FlatButton(
+                onPressed: () => {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                color: Colors.white,
+                child: Row(
+                  // Replace with a Row for horizontal icon + text
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+                    Icon(
+                      Icons.event_rounded,
+                      size: 60,
+                    ),
+                    Padding(padding: EdgeInsets.fromLTRB(45, 0, 0, 0)),
+                    Text(
+                      "Jobb",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            ),
+            //Button to get to Profile page
+            Container(
+              height: 100,
+              width: 330,
+              child: FlatButton(
+                onPressed: () => {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                color: Colors.white,
+                child: Row(
+                  // Replace with a Row for horizontal icon + text
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+                    Icon(
+                      Icons.person,
+                      size: 60,
+                    ),
+                    Padding(padding: EdgeInsets.fromLTRB(45, 0, 0, 0)),
+                    Text(
+                      "Profil",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            ),
+            //Button for previous jobs
+            Container(
+              height: 100,
+              width: 330,
+              child: FlatButton(
+                onPressed: () => {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                color: Colors.white,
+                child: Row(
+                  // Replace with a Row for horizontal icon + text
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.fromLTRB(10, 0, 0, 0)),
+                    Icon(
+                      Icons.done,
+                      size: 60,
+                    ),
+                    Padding(padding: EdgeInsets.fromLTRB(45, 0, 0, 0)),
+                    Text(
+                      "Historik",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+            ),
+            //Button for Accounts
+            Container(
+              height: 100,
+              width: 330,
+              child: FlatButton(
+                onPressed: () => {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
+                color: Colors.white,
+                child: Row(
+                  // Replace with a Row for horizontal icon + text
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
+                    Icon(
+                      Icons.person_add,
+                      size: 60,
+                    ),
+                    Padding(padding: EdgeInsets.fromLTRB(15, 0, 0, 0)),
+                    Text(
+                      "Hantera konton",
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
