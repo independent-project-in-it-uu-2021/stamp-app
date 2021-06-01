b4003a3eb61e8c4996071766d6179461d497ddcd
Ella Hallgren <ellahallgren@MacBook-Pro-som-tillhor-Daniel.local>
2021-06-01 11:12:30 +0200

    demonstration version klar ish

--- stamp_app/lib/studentScreens/studentJobs/studentjobs.dart
+++ stamp_app/lib/studentScreens/studentJobs/studentjobs.dart
@@ -1,37 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:stamp_app/screens/createJob/create-job.dart';
 import 'package:stamp_app/screens/editProfile/redigera-konto.dart';
+import 'package:stamp_app/screens/val/val.dart';
+import 'package:stamp_app/services/auth.dart';
 
-class Profil extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return ProfilState();
-  }
-}
-
-class ProfilState extends State<Profil> {
-  // State parameter
-  String _email;
-  String _userPassword;
-
-  // key to hold the state of the form i.e referens to the form
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  //TODO: Check if an email is already used
-
+class StudentJobs extends StatelessWidget {
+  final AuthService _firebaseAuth = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Image.asset('assets/images/uuLogaNew.png', fit: BoxFit.cover),
-        //centerTitle: true,
+        title: Text('Jobb'),
         backgroundColor: Colors.red.shade900,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_rounded),
-          //TODO: This does nothing now
-          onPressed: () => Navigator.of(context).pop(),
-          tooltip: 'Tillbaka',
-        ),
         actions: <Widget>[
           IconButton(
             padding: EdgeInsets.only(right: 10),
@@ -48,116 +28,21 @@ class ProfilState extends State<Profil> {
         width: double.infinity,
         child: SingleChildScrollView(
           child: Form(
-            key: _formKey,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 Padding(
-                  padding: EdgeInsets.only(top: 30),
+                  padding: EdgeInsets.only(top: 10),
                 ),
                 Text(
-                  'Din profil',
-                  textAlign: TextAlign.center,
+                  'Kommande jobb',
                   style: TextStyle(
-                    fontSize: 40,
+                    fontSize: 30,
                     color: Colors.black,
                   ),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 15),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 15),
-                ),
-                Text(
-                  'Förnamn Efternamn',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Image.asset(
-                  'assets/images/profilbild.png',
-                  fit: BoxFit.cover,
-                  scale: 1.5,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 12),
-                ),
-                Text(
-                  '+461234567',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 12),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 12),
-                ),
-                Text(
-                  'jane@xample.com',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 12),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 12),
-                ),
-                Text(
-                  'STAMP admin',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 12),
+                  padding: EdgeInsets.only(top: 10),
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -170,27 +55,125 @@ class ProfilState extends State<Profil> {
                 Padding(
                   padding: EdgeInsets.only(top: 30),
                 ),
+                TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 30),
+                      primary: Colors.black,
+                    ),
+                    onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Choice()),
+                          )
+                        },
+                    child: const Text('Jobb 1')),
                 Padding(
-                  padding: EdgeInsets.only(top: 150),
-                  child: TextButton(
-                      child: Text(
-                        'Redigera profil',
-                        style: TextStyle(
-                          fontSize: 20,
-                          decoration: TextDecoration.underline,
-                          color: Colors.black,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ProfileEdit()),
-                        );
-                      }),
+                  padding: EdgeInsets.only(top: 30),
                 ),
                 Padding(
-                  padding: EdgeInsets.only(top: 60),
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.001,
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    color: Colors.black12,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 30),
+                      primary: Colors.black,
+                    ),
+                    onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Choice()),
+                          )
+                        },
+                    child: const Text('Jobb 2')),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.001,
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    color: Colors.black12,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(0,50,0,0),),
+                Text(
+                  'Lediga jobb',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.001,
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    color: Colors.black12,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 30),
+                      primary: Colors.black,
+                    ),
+                    onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Choice()),
+                          )
+                        },
+                    child: const Text('Jobb 1')),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.001,
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    color: Colors.black12,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 30),
+                      primary: Colors.black,
+                    ),
+                    onPressed: () => {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Choice()),
+                          )
+                        },
+                    child: const Text('Jobb 2')),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.001,
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    color: Colors.black12,
+                  ),
                 ),
               ],
             ),
