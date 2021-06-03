225ceb63e4b2ad76138d115c7128a6b1e949d9df
Ella Hallgren <ellahallgren@MacBook-Pro-som-tillhor-Daniel.local>
2021-06-03 14:12:14 +0200

    fixat knappar för acceptera och reservera som ändrar färg när man klickar på dem.

--- stamp_app/lib/screens/val/val.dart
+++ stamp_app/lib/screens/val/val.dart
@@ -7,7 +7,21 @@ import 'package:stamp_app/screens/jobb/jobb.dart';
 import 'package:stamp_app/screens/slutval/slutval.dart';
 import 'package:stamp_app/screens/annansProfil/annansProfil.dart';
 
-class Choice extends StatelessWidget {
+bool _pressedButton1 = false;
+bool _pressedButton2 = false;
+bool _pressedButton3 = false;
+bool _pressedButton4 = false;
+bool _pressedButton5 = false;
+bool _pressedButton6 = false;
+
+class Choice extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ChoiceState();
+  }
+}
+
+class ChoiceState extends State<Choice> {
   final AuthService _firebaseAuth = AuthService();
   @override
   Widget build(BuildContext context) {
@@ -130,30 +144,109 @@ class Choice extends StatelessWidget {
                   padding: EdgeInsets.only(top: 30),
                 ),
                 //---------------
-                FlatButton(
-                  onPressed: () => {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                  color: Colors.white,
-                  child: Row(
-                    // Replace with a Row for horizontal icon + text
-                    children: <Widget>[
-                      Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
-                      Image.asset(
-                        'assets/images/profilbild.png',
-                        fit: BoxFit.cover,
-                        scale: 3,
-                      ),
-                      Padding(padding: EdgeInsets.fromLTRB(15, 0, 0, 0)),
-                      Text(
-                        'Kalle Hansson',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
+                Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 209,
+                      child: FlatButton(
+                        onPressed: () => {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0))),
+                        color: Colors.white,
+                        child: Row(
+                          // Replace with a Row for horizontal icon + text
+                          children: <Widget>[
+                            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
+                            Image.asset(
+                              'assets/images/profilbild.png',
+                              fit: BoxFit.cover,
+                              height: 45,
+                              width: 45,
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(5, 0, 0, 0)),
+                            Text(
+                              'Sixten Andersson',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 85,
+                          height: 35,
+                          child: RaisedButton(
+                            child: new Text(
+                              'Acceptera',
+                              style: TextStyle(
+                                  color: _pressedButton1
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.black, width: 2),
+                                borderRadius: new BorderRadius.circular(30.0)),
+                            textColor: Colors.white,
+
+                            // 2
+                            color: _pressedButton1
+                                ? Colors.red.shade900
+                                : Colors.white,
+
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _pressedButton1 = !_pressedButton1;
+                              })
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 5),
+                        ),
+                        Container(
+                          width: 85,
+                          height: 35,
+                          child: RaisedButton(
+                            child: new Text(
+                              'Reservera',
+                              style: TextStyle(
+                                  color: _pressedButton2
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.black, width: 2),
+                                borderRadius: new BorderRadius.circular(30.0)),
+                            textColor: Colors.white,
+
+                            // 2
+                            color: _pressedButton2
+                                ? Colors.red.shade900
+                                : Colors.white,
+
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _pressedButton2 = !_pressedButton2;
+                              })
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
 
                 Padding(
@@ -170,30 +263,109 @@ class Choice extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.only(top: 30),
                 ),
-                FlatButton(
-                  onPressed: () => {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                  color: Colors.white,
-                  child: Row(
-                    // Replace with a Row for horizontal icon + text
-                    children: <Widget>[
-                      Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
-                      Image.asset(
-                        'assets/images/profilbild.png',
-                        fit: BoxFit.cover,
-                        scale: 3,
-                      ),
-                      Padding(padding: EdgeInsets.fromLTRB(15, 0, 0, 0)),
-                      Text(
-                        'Agnes Brorson',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
+                Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 209,
+                      child: FlatButton(
+                        onPressed: () => {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0))),
+                        color: Colors.white,
+                        child: Row(
+                          // Replace with a Row for horizontal icon + text
+                          children: <Widget>[
+                            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
+                            Image.asset(
+                              'assets/images/profilbild.png',
+                              fit: BoxFit.cover,
+                              height: 45,
+                              width: 45,
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(5, 0, 0, 0)),
+                            Text(
+                              'Sixten Andersson',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 85,
+                          height: 35,
+                          child: RaisedButton(
+                            child: new Text(
+                              'Acceptera',
+                              style: TextStyle(
+                                  color: _pressedButton3
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.black, width: 2),
+                                borderRadius: new BorderRadius.circular(30.0)),
+                            textColor: Colors.white,
+
+                            // 2
+                            color: _pressedButton3
+                                ? Colors.red.shade900
+                                : Colors.white,
+
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _pressedButton3 = !_pressedButton3;
+                              })
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 5),
+                        ),
+                        Container(
+                          width: 85,
+                          height: 35,
+                          child: RaisedButton(
+                            child: new Text(
+                              'Reservera',
+                              style: TextStyle(
+                                  color: _pressedButton4
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.black, width: 2),
+                                borderRadius: new BorderRadius.circular(30.0)),
+                            textColor: Colors.white,
+
+                            // 2
+                            color: _pressedButton4
+                                ? Colors.red.shade900
+                                : Colors.white,
+
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _pressedButton4 = !_pressedButton4;
+                              })
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
                 Padding(
                   padding: EdgeInsets.only(top: 30),
@@ -209,31 +381,111 @@ class Choice extends StatelessWidget {
                 Padding(
                   padding: EdgeInsets.only(top: 30),
                 ),
-                FlatButton(
-                  onPressed: () => {},
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                  color: Colors.white,
-                  child: Row(
-                    // Replace with a Row for horizontal icon + text
-                    children: <Widget>[
-                      Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
-                      Image.asset(
-                        'assets/images/profilbild.png',
-                        fit: BoxFit.cover,
-                        scale: 3,
-                      ),
-                      Padding(padding: EdgeInsets.fromLTRB(15, 0, 0, 0)),
-                      Text(
-                        'Sixten Andersson',
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
+                Row(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 209,
+                      child: FlatButton(
+                        onPressed: () => {},
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0))),
+                        color: Colors.white,
+                        child: Row(
+                          // Replace with a Row for horizontal icon + text
+                          children: <Widget>[
+                            Padding(padding: EdgeInsets.fromLTRB(0, 0, 0, 0)),
+                            Image.asset(
+                              'assets/images/profilbild.png',
+                              fit: BoxFit.cover,
+                              height: 45,
+                              width: 45,
+                            ),
+                            Padding(padding: EdgeInsets.fromLTRB(5, 0, 0, 0)),
+                            Text(
+                              'Sixten Andersson',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 85,
+                          height: 35,
+                          child: RaisedButton(
+                            child: new Text(
+                              'Acceptera',
+                              style: TextStyle(
+                                  color: _pressedButton5
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.black, width: 2),
+                                borderRadius: new BorderRadius.circular(30.0)),
+                            textColor: Colors.white,
+
+                            // 2
+                            color: _pressedButton5
+                                ? Colors.red.shade900
+                                : Colors.white,
+
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _pressedButton5 = !_pressedButton5;
+                              })
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 5),
+                        ),
+                        Container(
+                          width: 85,
+                          height: 35,
+                          child: RaisedButton(
+                            child: new Text(
+                              'Reservera',
+                              style: TextStyle(
+                                  color: _pressedButton6
+                                      ? Colors.white
+                                      : Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            shape: RoundedRectangleBorder(
+                                side: BorderSide(color: Colors.black, width: 2),
+                                borderRadius: new BorderRadius.circular(30.0)),
+                            textColor: Colors.white,
+
+                            // 2
+                            color: _pressedButton6
+                                ? Colors.red.shade900
+                                : Colors.white,
+
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _pressedButton6 = !_pressedButton6;
+                              })
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
+
                 Padding(
                   padding: EdgeInsets.only(top: 30),
                 ),
