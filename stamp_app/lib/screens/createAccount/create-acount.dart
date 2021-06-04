3c702bdcc2fde7d18bfa1dd476e96100e7d93632
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2021-06-04 10:51:48 +0200

    User can upload image, if no pic is choosen default profil pic is used

--- stamp_app/lib/screens/createAccount/create-acount.dart
+++ stamp_app/lib/screens/createAccount/create-acount.dart
@@ -25,8 +25,10 @@ class CreateAccountState extends State<CreateAccount> {
   String _userPassword;
   String _chosenProgram;
   String _errorMsg = '';
+  String _profileImageUrl;
+  bool _useDefaultPic = true;
   String _defaultProfilePic =
-      'gs://stamp-db6ad.appspot.com/userProfilePicture/defaultPicChangeLater.jpg';
+      'https://firebasestorage.googleapis.com/v0/b/stamp-db6ad.appspot.com/o/userProfilePicture%2Fprofile-user.png?alt=media&token=170630ba-bccf-4bd7-b50b-9d5e08f01a73';
   String _defaultAccountStatue = 'inactive';
   File _userImage;
   // Boolean value use to hide the write bio option field
@@ -34,15 +36,6 @@ class CreateAccountState extends State<CreateAccount> {
 
   // Authentication instance used to login
   final AuthService _auth = AuthService();
-
-  Future _getImage() async {
-    final pickedImage =
-        await ImagePicker().getImage(source: ImageSource.gallery);
-    setState(() {
-      _userImage = File(pickedImage.path);
-      print('_UserImage: $_userImage');
-    });
-  }
 
   // Method that is used to change the margin when an image is choosen
   double _changeMarginImage() {
@@ -59,11 +52,35 @@ class CreateAccountState extends State<CreateAccount> {
     return curMargin;
   }
 
+  // Getting the image from the phone
+  Future _getImage() async {
+    final pickedImage =
+        await ImagePicker().getImage(source: ImageSource.gallery);
+    _useDefaultPic = false;
+    setState(() {
+      _userImage = File(pickedImage.path);
+      print('_UserImage: $_userImage');
+    });
+  }
+
   //Upload the image to firebase storage
   Future uploadImage(BuildContext context) async {
+    print('inside uploadImage');
+    // Path of the picture
     String fileName = basename(_userImage.path);
+
+    // Referene to the firebase storage or reference to the bucket
     Reference storageReference =
-        FirebaseStorage.instance.ref().child('userProfilePicture/&fileName');
+        FirebaseStorage.instance.ref().child('userProfilePicture/$fileName');
+
+    // Put the file in the bucket above
+    final UploadTask uploadTask = storageReference.putFile(_userImage);
+    // Getting image url
+    final TaskSnapshot downloadUrl = (await uploadTask);
+    // Url of the file in the storagew
+    final String imageUrl = await downloadUrl.ref.getDownloadURL();
+    return imageUrl;
+    print(imageUrl);
   }
 
   void showWriteBio() {
@@ -71,8 +88,6 @@ class CreateAccountState extends State<CreateAccount> {
       _writeBio = !_writeBio;
     });
   }
-
-  //final programList<String> = ['Elektroteknik', 'Energisystem', 'Industriell ekonomi'];
 
   // key to hold the state of the form i.e referens to the form
   // this is a global form key
@@ -172,11 +187,11 @@ class CreateAccountState extends State<CreateAccount> {
         validator: (String value) {
           if (value.isEmpty) {
             return 'Lösenord är obligatorisk';
+          } else if (value.length < 6) {
+            return 'Lösenord behöver vara minst 6 tecken';
           }
           return null;
         },
-        //TODO: Password requirements
-        // The  form is saved and we tell what to do with the value
         onChanged: (String value) {
           setState(() {
             _userPassword = value;
@@ -201,65 +216,13 @@ class CreateAccountState extends State<CreateAccount> {
         validator: (String value) {
           if (value.isEmpty) {
             return 'Upprepa lösenord är obligatorisk';
-          }
-          if (value != _userPassword) {
-            print('Value inside checkpassword');
-            print(value);
+          } else if (value != _userPassword) {
             return 'Vänligen ange samma lösenord';
+          } else if (value.length < 6) {
+            return 'Lösenord behöver vara minst 6 tecken';
           }
           return null;
         },
-      ),
-    );
-  }
-
-  // Choose program from dropdown
-  Widget _program() {
-    return Container(
-      width: 350,
-      height: 60,
-      child: DropdownButtonFormField(
-        //TODO: Change the position of the list to above
-        items: <String>[
-          'IT',
-          'STS',
-          'F',
-          'W',
-        ].map<DropdownMenuItem<String>>((String value) {
-          return new DropdownMenuItem(
-            value: value,
-            child: Row(
-              children: <Widget>[
-                //TODO: Check with the grupp maybe change the icon
-                Icon(Icons.arrow_right),
-                Text(value),
-              ],
-            ),
-          );
-        }).toList(),
-        onChanged: (String newValue) {
-          print('Inside dropdown');
-          print(newValue);
-          if (newValue == 'Övrigt') {
-            showWriteBio();
-          } else {
-            //showWriteBio();
-            setState(() {
-              _chosenProgram = newValue;
-            });
-          }
-        },
-        decoration: InputDecoration(
-            border: new OutlineInputBorder(
-                borderRadius:
-                    const BorderRadius.all(const Radius.circular(30.0))),
-            contentPadding: EdgeInsets.only(left: 15, top: 15),
-            errorStyle: TextStyle(color: Colors.white, fontSize: 13),
-            filled: true,
-            fillColor: Colors.white,
-            hintText: 'Program',
-            //TODO: Change the text below
-            errorText: 'Vänligen välj en program'),
       ),
     );
   }
@@ -268,38 +231,34 @@ class CreateAccountState extends State<CreateAccount> {
   Widget _buildBio() {
     return Container(
       width: 350,
-      child: Visibility(
-        visible: _writeBio,
-        child: TextFormField(
-          keyboardType: TextInputType.name,
-          // Decorate the input field here,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-
-            //TODO: Change the text below
-            hintText: 'Din roll?',
-            counterStyle: TextStyle(color: Colors.white),
-            errorStyle: TextStyle(color: Colors.white),
-          ),
-          // The acutal value from the input
-          validator: (String value) {
-            if (value.isEmpty) {
-              return 'Bio är obligatorisk';
-            }
-            if (value.length > 150) {
-              //TODO: Change the text below
-              return 'Bio får inte vara längre än 120 tecken';
-            }
-            return null;
-          },
-          // The  form is saved and we tell what to do with the value
-          onChanged: (String value) {
-            setState(() {
-              _chosenProgram = value;
-            });
-          },
+      child: TextFormField(
+        keyboardType: TextInputType.name,
+        maxLength: 300,
+        // Decorate the input field here,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(15, 10, 0, 40),
+          filled: true,
+          fillColor: Colors.white,
+          hintText: 'Kort bio',
+          counterStyle: TextStyle(color: Colors.white),
+          errorStyle: TextStyle(color: Colors.white),
         ),
+        // The acutal value from the input
+        validator: (String value) {
+          if (value.isEmpty) {
+            return 'Bio är obligatorisk';
+          }
+          if (value.length > 300) {
+            return 'Bio får inte vara längre än 300 tecken';
+          }
+          return null;
+        },
+        // The  form is saved and we tell what to do with the value
+        onChanged: (String value) {
+          setState(() {
+            _chosenProgram = value;
+          });
+        },
       ),
     );
   }
@@ -365,37 +324,38 @@ class CreateAccountState extends State<CreateAccount> {
                 Padding(
                   padding: EdgeInsets.only(top: 12),
                 ),
-                _program(),
+                /*_program(),
                 Padding(
                   padding: EdgeInsets.only(top: 12),
-                ),
+                ),*/
                 _buildBio(),
                 // adding space
                 SizedBox(
                   height: 12,
                 ),
                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    alignment: Alignment.center,
-                    //padding: EdgeInsets.only(top: 10),
-                  ),
-                  child: Container(
-                    height: 60,
-                    width: 100,
-                    margin: EdgeInsets.only(top: _changeMarginImage()),
-                    child: _userImage == null
-                        ? Text(
-                            'Ladda upp profilbild',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.black,
-                            ),
-                          )
-                        : Image.file(_userImage),
-                  ),
-                  onPressed: _getImage,
-                ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      alignment: Alignment.center,
+                      //padding: EdgeInsets.only(top: 10),
+                    ),
+                    child: Container(
+                      height: 60,
+                      width: 100,
+                      margin: EdgeInsets.only(top: _changeMarginImage()),
+                      child: _userImage == null
+                          ? Text(
+                              'Ladda upp profilbild',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            )
+                          : Image.file(_userImage),
+                    ),
+                    onPressed: () {
+                      _getImage();
+                    }),
                 Padding(
                   padding: EdgeInsets.only(top: 12),
                 ),
@@ -421,14 +381,19 @@ class CreateAccountState extends State<CreateAccount> {
                     // If the form is valid, onSaved method is called
                     // onsave method from above is called
                     if (_formKey.currentState.validate()) {
+                      _useDefaultPic
+                          ? _profileImageUrl = _defaultProfilePic
+                          : _profileImageUrl = await uploadImage(context);
+
                       dynamic result = await _auth.registerWithEmailAndPassword(
                           _email,
                           _userPassword,
                           _fullName,
                           _mobilnumber,
                           _chosenProgram,
-                          _defaultProfilePic,
+                          _profileImageUrl,
                           _defaultAccountStatue);
+                      print(result);
                       if (result == null) {
                         setState(() {
                           _errorMsg = 'Mejladressen används redan';
