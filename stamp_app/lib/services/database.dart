8fdf4c20308ac7faa7f5cab98f2b2f30ba0d7ed1
Daniel <danno-082011@hotmail.com>
2021-06-09 09:52:30 +0200

    added db function for updating user

--- stamp_app/lib/services/database.dart
+++ stamp_app/lib/services/database.dart
@@ -32,8 +32,94 @@ class DatabaseService {
     );
   }
 
+//Updating the accountType of a user
+  Future updateStatus(String accountType) async {
+    if (accountType == 'student') {
+      return await userCollection.doc(userId).set(
+        {
+          'accountType': 'admin',
+        },
+      );
+    }
+    if (accountType == 'admin') {
+      return await userCollection.doc(userId).set(
+        {
+          'accountType': 'student',
+        },
+      );
+    }
+    if (accountType == 'inactive') {
+      return await userCollection.doc(userId).set(
+        {
+          'accountType': 'student',
+        },
+      );
+    } else {
+      return await userCollection.doc(userId).set(
+        {
+          'accountType': accountType,
+        },
+      );
+    }
+  }
+
   // Get user information stream
   Stream<DocumentSnapshot> get userData {
     return userCollection.doc(userId).snapshots();
   }
 }
+         
+//final String documentId;
+//GetUserName(this.documentId);
+/*
+_getUser(context) {
+  return FutureBuilder<DocumentSnapshot>(
+    future: users.doc('users').get(),
+    builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+      if (snapshot.hasError) {
+        return Text("Something went wrong");
+      }
+
+      if (snapshot.hasData && !snapshot.data.exists) {
+        return Text("Document does not exist");
+      }
+
+      if (snapshot.connectionState == ConnectionState.done) {
+        Map<String, dynamic> data = snapshot.data.data();
+        print(data);
+        return Text("Full Name: ${data['userName']} ${data['userBio']}");
+      }
+
+      return Text("loading");
+    },
+  );
+  print(users);
+
+  setState(() {
+    //getIsNewUSer=document['IsNewUser'];
+    print(users);
+  });
+
+  /*FirebaseFirestore.instance
+        .document('users/$uid')
+        .get()
+        .then((DocumentSnapshot document) {
+      print("document_build:$document");
+      setState(() {
+        getIsNewUSer = document['accountType'];
+        print("getIsNewUSe:$getIsNewUSer");
+      });
+    });*/
+}
+/*
+  _makeAdmin() {
+    FirebaseFirestore.instance
+        .collection('users')
+        .document(uid)
+        .updateData({"accountType": "admin"}).then((result) {
+      print("User is now admin");
+    }).catchError((onError) {
+      print("onError");
+    });
+  }*/
+  */
