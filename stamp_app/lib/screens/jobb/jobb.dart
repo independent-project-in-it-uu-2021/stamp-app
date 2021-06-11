46b80667665be39e1118c0cd9f8e13383ea10684
Emanuel Jansson <Emanuel.Jansson.9213@student.uu.se>
2021-06-11 09:30:20 +0200

    Tiles för kommande jobb

--- stamp_app/lib/screens/jobb/jobb.dart
+++ stamp_app/lib/screens/jobb/jobb.dart
@@ -6,6 +6,32 @@ import 'package:stamp_app/services/auth.dart';
 import 'package:stamp_app/studentScreens/studentJobs/studentjobs.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+var icon = ''; //TODO: oklart hur detta ska fungera
+var title = 'Lego Workshop';
+var date = '2017-07-11';
+var time = '13:00 - 15:00';
+var location = 'Uppsala';
+var count = '3/4 st';
+
+//Detta ska egentligen hämtas från databasen
+List<Map<String, dynamic>> _menuItem = [
+  {
+    'title': '$title',
+    'icon': Icon(Icons.home),
+    'subtitle': '$date \n$time \n$location \n$count',
+  },
+  {
+    'title': 'Teams',
+    'icon': Icon(Icons.people),
+    'subtitle': '$date \n$time \n$location \n$count',
+  },
+  {
+    'title': 'Test',
+    'icon': Icon(Icons.lightbulb),
+    'subtitle': '$date \n$time \n$location \n$count',
+  },
+];
+
 class Work extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,314 +51,27 @@ class Work extends StatelessWidget {
           ),
         ],*/
       ),
-      body: Container(
-        width: double.infinity,
-        child: SingleChildScrollView(
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                //FirebaseFirestore.instance.collection('jobs').Get(),
-                Padding(
-                  padding: EdgeInsets.only(top: 30),
-                ),
-                Text(
-                  'Kommande jobb',
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.black,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 30),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 30),
-                    primary: Colors.black,
-                  ),
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Choice()),
-                    )
-                  },
-                  child: Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 30)),
-                      Icon(
-                        Icons.smart_toy,
-                        size: 70,
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 20)),
-                      Column(
-                        children: [
-                          Text(
-                            'Lego workshop',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                            '2021-07-11',
-                            style: TextStyle(fontSize: 20),
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            '13:00 - 15:00',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            'Uppsala',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            '3/4 st',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 30),
-                    primary: Colors.black,
-                  ),
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Choice()),
-                    )
-                  },
-                  child: Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 30)),
-                      Icon(
-                        Icons.smart_toy_outlined,
-                        size: 70,
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 20)),
-                      Column(
-                        children: [
-                          Text(
-                            'Lego Workshop',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                            '2021-08-01',
-                            style: TextStyle(fontSize: 20),
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            '11:00 - 15:00',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            'Uppsala',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            '1/3 st',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 30),
-                    primary: Colors.black,
-                  ),
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Choice()),
-                    )
-                  },
-                  child: Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 30)),
-                      Icon(
-                        Icons.dining,
-                        size: 70,
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 10)),
-                      Column(
-                        children: [
-                          Text(
-                            'Lunchföreläsning',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                            '2021-10-21',
-                            style: TextStyle(fontSize: 20),
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            '12:15 - 13:00',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            'Uppsala',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            '1/2 st',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 30),
-                    primary: Colors.black,
-                  ),
-                  onPressed: () => {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Choice()),
-                    )
-                  },
-                  child: Row(
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 30)),
-                      Icon(
-                        Icons.smart_toy,
-                        size: 70,
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 20)),
-                      Column(
-                        children: [
-                          Text(
-                            'Lego workshop',
-                            style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Text(
-                            '2021-12-11',
-                            style: TextStyle(fontSize: 20),
-                            textAlign: TextAlign.left,
-                          ),
-                          Text(
-                            '13:00 - 15:00',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            'Uppsala',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            '3/4 st',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.001,
-                    width: MediaQuery.of(context).size.width * 0.83,
-                    color: Colors.black12,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FloatingActionButton(
-              backgroundColor: Colors.red.shade900,
-              foregroundColor: Colors.white,
-              onPressed: () => {
+      body: new ListView.builder(
+        itemCount: _menuItem.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+            child: ListTile(
+              leading: _menuItem[index]['icon'],
+              title: Text(_menuItem[index]['title']),
+              subtitle: Text(_menuItem[index]['subtitle']),
+              /*child: Align(
+                alignment: Alignment.topRight,
+                child: Text('TESTING TESTING'),
+              ),*/
+              onTap: () => {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => CreateJob()),
+                  MaterialPageRoute(builder: (context) => Choice()),
                 )
               },
-              child: Icon(Icons.add),
             ),
-            Padding(padding: EdgeInsets.only(bottom: 100))
-          ],
-        ),
+          );
+        },
       ),
     );
   }
